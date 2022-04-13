f18986ace09e73150187d51a3821719eba79cae0
Med <meddhif@gmail.com>
2022-04-13 14:26:23 +0700

    Update ProductsApplication.java

--- src/main/java/com/springdata/products/ProductsApplication.java
+++ src/main/java/com/springdata/products/ProductsApplication.java
@@ -2,6 +2,9 @@ package com.springdata.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
 public class ProductsApplication extends SpringBootServletInitializer  {
