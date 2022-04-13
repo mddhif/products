77acf1ff3307dccc8a488b3adf3676f276d80400
Med <meddhif@gmail.com>
2022-04-13 14:22:39 +0700

    Update ProductsApplication.java

--- src/main/java/com/springdata/products/ProductsApplication.java
+++ src/main/java/com/springdata/products/ProductsApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductsApplication {
+public class ProductsApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsApplication.class, args);
 	}
+	
+	 @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+            return builder.sources(ProductsApplication.class);
+  }
 
 }
